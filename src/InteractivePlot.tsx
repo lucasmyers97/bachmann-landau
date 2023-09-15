e5c08fb4791d6b940d55ed167dec7af546311ddc
Lucas Myers <lucasmyers97@gmail.com>
2023-09-14 22:50:07 -0500

    Added touch zoom to app

--- src/InteractivePlot.tsx
+++ src/InteractivePlot.tsx
@@ -54,12 +54,65 @@ function InteractivePlot(props: InteractivePlotProps) {
     return `Scale: ${Number(scale).toExponential(2)}`;
   }
 
+  // Handle pinch zoom
+  const [touch_cache, setTouchCache] = React.useState<React.PointerEvent<HTMLInputElement>[]>([]);
+  const [pointer_distance, setPointerDistance] = React.useState(0);
+
+  function dist(x1: number, x2: number, y1: number, y2: number) {
+      return Math.sqrt((x1 - x2)**2 + (y1 - y2)**2);
+  }
+
+  function pointerDownHandler(e: React.PointerEvent<HTMLInputElement>) {
+      const new_touch_cache = touch_cache.concat(e);
+      setTouchCache(new_touch_cache);
+      if (new_touch_cache.length === 2) {
+          setPointerDistance( dist(new_touch_cache[0].clientX,
+                                   new_touch_cache[1].clientX,
+                                   new_touch_cache[0].clientY,
+                                   new_touch_cache[1].clientY) );
+      }
+  }
+
+  function pointerMoveHandler(e: React.PointerEvent<HTMLInputElement>) {
+    const new_touch_cache = touch_cache.map(v => {
+            if (v.pointerId === e.pointerId) {
+                return e;
+            } else {
+                return v;
+            }
+        });
+    setTouchCache(new_touch_cache);
+
+    // If two pointers are down, check for pinch gestures
+    if (touch_cache.length === 2) {
+      // Calculate the distance between the two pointers
+      const new_pointer_distance = dist(touch_cache[0].clientX,
+                                        touch_cache[1].clientX,
+                                        touch_cache[0].clientY,
+                                        touch_cache[1].clientY) 
+
+      setZoom( updateZoom(zoom, new_pointer_distance - pointer_distance) );
+
+      setPointerDistance(new_pointer_distance);
+    }
+  }
+
+  function pointerUpHandler(e: React.PointerEvent<HTMLInputElement>) {
+    const new_touch_cache = touch_cache.filter(v => v.pointerId !== e.pointerId);
+    setTouchCache(new_touch_cache);
+  }
 
   // Actual component
   return (
     <div 
       className={styles.InteractivePlot}
-      onWheelCapture={handleScroll}>
+      onWheelCapture={handleScroll}
+      onPointerUp={pointerUpHandler}
+      onPointerCancel={pointerUpHandler}
+      onPointerLeave={pointerUpHandler}
+      onPointerOut={pointerUpHandler}
+      onPointerDown={pointerDownHandler}
+      onPointerMove={pointerMoveHandler}>
       <Plot 
         zoom={zoom}
         paths={props.paths}
